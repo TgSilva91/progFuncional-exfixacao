0b99ed7f0200fee9cc281eee27aa0cd4a8c1e6af
Tiago Silva <tgsilva1991@gmail.com>
2022-05-25 00:10:09 +0100

    Created main class Program. Exercise finished

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,67 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Employee> list = new ArrayList<>();
+
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		System.out.print("Enter salary: ");
+		Double salary = sc.nextDouble();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+			}
+
+			System.out.println("Email of people whose salary is more than 2000.00:");
+
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			List<String> email = list.stream()
+					.filter(p -> p.getSalary() > salary)
+					.map(p -> p.getEmail())
+					.sorted(comp)
+					.collect(Collectors.toList());
+			
+			email.forEach(System.out::println);
+			
+			System.out.print("Sum of salary of people whose name starts with 'M': ");
+			
+			double sumSalary = list.stream()
+									.filter(p -> p.getName().toUpperCase().charAt(0) == 'M')
+									.map(p -> p.getSalary())
+									.reduce(0.0, (x,y) -> x + y);
+
+			System.out.println(sumSalary);
+
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		sc.close();
 	}
 
 }
